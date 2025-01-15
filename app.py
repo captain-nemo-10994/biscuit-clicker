7181e115f1f19423be0760c36982d7eb5beb383d
Samuel <samueladams@letu.edu>
2025-01-15 09:00:10 -0600

    First wave of refactoring.

--- app.py
+++ app.py
@@ -5,62 +5,51 @@ app = Flask(__name__)
 click_power = 1
 auto_click = 0
 
+def get_biscuits():
+    return int(request.json.get("biscuits"))
+
+def get_click_cost_val():
+    return int(request.json.get("clickCostVal"))
+
+def get_auto_cost_val():
+    return int(request.json.get("autoCostVal"))
+
 @app.route("/increment", methods=["POST"])
 def increment():
     global click_power
-    biscuits = request.json.get("biscuits")
-    biscuits = int(biscuits)
-
-    print("Old number: " + str(biscuits))
-
+    biscuits = get_biscuits()
     biscuits = biscuits + click_power
-
-    print("New number: " + str(biscuits))
-
     return {"answer": biscuits}
 
 @app.route("/oneUp", methods=["POST"])
 def oneUp():
     global click_power
-    biscuits = request.json.get("biscuits")
-    biscuits = int(biscuits)
-
-    clickCostVal = request.json.get("clickCostVal")
-    clickCostVal = int(clickCostVal)
-
+    biscuits = get_biscuits()
+    clickCostVal = get_click_cost_val()
     if biscuits >= clickCostVal:
         click_power = click_power + 1
         return {"biscuits": biscuits-clickCostVal, "clickCostVal":clickCostVal+(click_power*click_power)}
     else:
-        print("Not enough biscuits")
         return{"biscuits":biscuits, "clickCostVal":clickCostVal}
 
 @app.route("/buyAutoClick", methods=["POST"])
 def buyAutoClick():
     global auto_click
-    biscuits = request.json.get("biscuits")
-    biscuits = int(biscuits)
-
-    autoCostVal = request.json.get("autoCostVal")
-    autoCostVal = int(autoCostVal)
-
+    biscuits = get_biscuits()
+    autoCostVal = get_auto_cost_val()
     if biscuits >= autoCostVal:
         auto_click = auto_click + 1
         return {"biscuits": biscuits-autoCostVal, "autoCostVal":autoCostVal+(auto_click*auto_click)}
     else:
-        print("Not enough biscuits")
         return {"biscuits":biscuits, "autoCostVal":autoCostVal}
 
 @app.route("/autoClick", methods=["POST"])
 def autoClick():
     global auto_click
-    biscuits = request.json.get("biscuits")
-    biscuits = int(biscuits)
-
+    biscuits = get_biscuits()
     biscuits = biscuits + auto_click
     return {"biscuits":biscuits}
 
 @app.route("/")
 def hello_world():
-
     return render_template("index.html")
